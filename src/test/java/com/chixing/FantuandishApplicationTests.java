9f33f653a600f301d3a7e8908053d66eb86b9f10
baicha1023 <1750929495@qq.com>
2022-09-30 15:41:21 +0800

    zhangxu9.30_15.35

--- src/test/java/com/chixing/FantuandishApplicationTests.java
+++ src/test/java/com/chixing/FantuandishApplicationTests.java
@@ -1,13 +1,19 @@
 package com.chixing;
 
+import com.chixing.entity.Coupon;
+import com.chixing.mapper.CouponMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FantuandishApplicationTests {
-
+    @Autowired
+    CouponMapper couponMapper;
     @Test
     void contextLoads() {
+        Coupon coupon = couponMapper.selectById(10);
+        System.out.println(coupon);
     }
 
 }
