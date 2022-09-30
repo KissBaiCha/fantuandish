9bc41bad2c6ff2355303f343bc23ef127893f6bb
baicha1023 <1750929495@qq.com>
2022-09-30 14:04:41 +0800

    zhangxu

---
+++ src/main/java/com/chixing/FantuandishApplication.java
@@ -0,0 +1,13 @@
+package com.chixing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantuandishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FantuandishApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chixing/FantuandishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chixing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantuandishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
