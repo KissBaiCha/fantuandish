1d0f25179ca6499014759e65be4b40acb89defe8
Asphyxia <1628776120@qq.com>
2022-10-25 19:54:30 +0800

    shangjian10.25_19:54

--- src/main/java/com/chixing/controller/ShopController.java
+++ src/main/java/com/chixing/controller/ShopController.java
@@ -37,27 +37,56 @@ public class ShopController {
         return shopService.remove(shopId);
     }
 
+    /**
+     * 条件筛选（食品类型）
+     * @param foodType  食品类型
+     * @return  店铺集合
+     */
     @GetMapping("/shop/foodType")
     private List<Shop> getByFoodType(@RequestParam("foodType")String foodType){
         List<Shop> shopList = shopService.getByFoodType(foodType);
         shopList.forEach(System.out::println);
         return shopList;
     }
+
+    /**
+     * 条件筛选（人均消费）
+     * @param shopMinCost   最低人均消费
+     * @param shopMaxCost   最高人均消费
+     * @return  店铺集合
+     */
     @GetMapping("/shop/shopAvgCost")
     private List<Shop> getByShopAvgCost(@RequestParam("shopMinCost")Float shopMinCost,@RequestParam("shopMaxCost")Float shopMaxCost){
         List<Shop> shopList = shopService.getByShopAvgCost(shopMinCost,shopMaxCost);
         return shopList;
     }
+
+    /**
+     * 多条件筛选（食品类型，人均消费）
+     * @param foodType      食品类型
+     * @param shopMinCost   最低人均消费
+     * @param shopMaxCost   最高人均消费
+     * @return  店铺集合
+     */
     @GetMapping("/shop/sift")
     private List<Shop> getBySift(@RequestParam("foodType")String foodType,@RequestParam("shopMinCost")Float shopMinCost,@RequestParam("shopMaxCost")Float shopMaxCost){
         List<Shop> shopList = shopService.getBySift(foodType,shopMinCost,shopMaxCost);
         return shopList;
     }
+
+    /**
+     * 排序（价格降序）
+     * @return  店铺集合
+     */
     @GetMapping("/shop/getByPrice")
     public List<Shop> getByPrice(){
         return shopService.getByPrice();
     }
 
+    /**
+     * 排序（评分降序）
+     * @return  店铺集合
+     */
     @GetMapping("/shop/getByScore")
     public List<Shop> getByScore(){
         return shopService.getByScore();
