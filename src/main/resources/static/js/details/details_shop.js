1a703e886a08ceff6bc30e0d65d30fb5487575b0
Asphyxia <1628776120@qq.com>
2022-10-31 10:15:01 +0800

    shangjian10.31_10:14

--- src/main/resources/static/js/details/details_shop.js
+++ src/main/resources/static/js/details/details_shop.js
@@ -11,11 +11,11 @@ document.querySelector(".closemap").onclick=function(){
 var onclicktime = 0;
 document.querySelector(".collect").onclick=function(){
     if(onclicktime % 2 == 0){
-        document.querySelector(".collect").firstChild.style.backgroundImage = "url(http://zhangxu-1023.oss-cn-nanjing.aliyuncs.com/images/details_shop/2022-10-20/82a76dec10f94b98846b487e8e766b26shop-collect-after.svg?Expires=1666631627&OSSAccessKeyId=LTAI5tG7nJQaHNGWeSvwmYn9&Signature=eLAX1vrOdljoUeEDZIjf55ox38U%3D)";
+        document.querySelector(".collect").firstChild.style.backgroundImage = "url(https://zhangxu-1023.oss-cn-nanjing.aliyuncs.com/images/details_shop/2022-10-20/82a76dec10f94b98846b487e8e766b26shop-collect-after.svg)";
         document.querySelector(".collect-info").innerText = "已收藏";
     }
     if(onclicktime % 2 == 1){
-        document.querySelector(".collect").firstChild.style.backgroundImage = "url(http://zhangxu-1023.oss-cn-nanjing.aliyuncs.com/images/details_shop/2022-10-20/0c33e04baf2d42a88d794b423fa6fab5shop-collect-before.svg?Expires=1666631614&OSSAccessKeyId=LTAI5tG7nJQaHNGWeSvwmYn9&Signature=jX%2FGXl4ddw0aYSB7YhnHVP9o%2FPk%3D)";
+        document.querySelector(".collect").firstChild.style.backgroundImage = "url(images/details_shop/2022-10-20/0c33e04baf2d42a88d794b423fa6fab5shop-collect-before.svg)";
         document.querySelector(".collect-info").innerText = "收藏";
     }
     onclicktime++;
