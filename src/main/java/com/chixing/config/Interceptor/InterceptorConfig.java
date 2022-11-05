50162a305c57a59143388df963060b674874d919
Asphyxia <1628776120@qq.com>
2022-11-05 16:12:55 +0800

    shangjian11.05_16:12

--- src/main/java/com/chixing/config/Interceptor/InterceptorConfig.java
+++ src/main/java/com/chixing/config/Interceptor/InterceptorConfig.java
@@ -1,22 +1,22 @@
-package com.chixing.config.Interceptor;
-
-import com.chixing.util.TokenInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-/**
- * @author Xu Zhang
- * @version 1.0
- * @date 2022/10/19 18:28
- */
-@Configuration
-public class InterceptorConfig implements WebMvcConfigurer {
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new TokenInterceptor())
-//                进行拦截，一般登录不拦截，企业都拦截
-                .addPathPatterns("/customer/**")
-                .excludePathPatterns("/customer/login");
-    }
-}
+//package com.chixing.config.Interceptor;
+//
+//import com.chixing.util.TokenInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+///**
+// * @author Xu Zhang
+// * @version 1.0
+// * @date 2022/10/19 18:28
+// */
+//@Configuration
+//public class InterceptorConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(new TokenInterceptor())
+////                进行拦截，一般登录不拦截，企业都拦截
+//                .addPathPatterns("/customer/**")
+//                .excludePathPatterns("/customer/login");
+//    }
+//}
